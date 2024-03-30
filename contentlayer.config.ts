01219c16c69d673a36a13e339717c4ffcea152e1
maxwiseman <max@maxwiseman.io>
2024-03-30 00:34:41 -0400

    initial: init t3 app and setup basic dependencies

---
+++ contentlayer.config.ts
@@ -0,0 +1,30 @@
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+// import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "date", required: true },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "src/content/posts",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+  },
+});
---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,28 @@
+// app/posts/[slug]/page.tsx
+import { Mdx } from "@/components/mdx";
+import { allPosts } from "contentlayer/generated";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post._raw.flattenedPath,
+  }));
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<React.ReactElement> {
+  // Find the post for the current page.
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+
+  // 404 if the post does not exist.
+  if (!post) notFound();
+
+  return (
+    <div>
+      <Mdx code={post.body.code} />
+    </div>
+  );
+}
